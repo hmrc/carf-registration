78564347ce6d2fc3aba869eedebe986b36abf833
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2025-11-03 15:57:55 +0000

    CARF-240 controller tests

--- test/controllers/RegistrationControllerSpec.scala
+++ test/controllers/RegistrationControllerSpec.scala
@@ -19,11 +19,14 @@ package controllers
 import base.SpecBase
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{reset, when}
+import play.api.http.Status.{INTERNAL_SERVER_ERROR, NOT_FOUND, OK}
 import play.api.libs.json.{JsValue, Json}
-import play.api.mvc.Results.{BadRequest, Ok}
+import play.api.mvc.Results.BadRequest
+import play.api.test.Helpers.{contentAsJson, contentAsString, status}
 import uk.gov.hmrc.carfregistration.controllers.RegistrationController
-import uk.gov.hmrc.carfregistration.models.requests.{IndividualDetails, RegisterIndWithIdAPIRequest, RegisterIndWithIdFrontendRequest, RequestCommon, RequestDetailIndividual}
-import uk.gov.hmrc.carfregistration.models.responses.{AddressResponse, IndividualResponse, RegisterIndWithIdAPIResponse, ResponseCommon, ResponseDetail}
+import uk.gov.hmrc.carfregistration.models.requests.RegisterIndWithIdFrontendRequest
+import uk.gov.hmrc.carfregistration.models.responses.{AddressResponse, RegisterIndWithIdFrontendResponse}
+import uk.gov.hmrc.carfregistration.models.{InternalServerError, NotFoundError}
 import uk.gov.hmrc.carfregistration.services.RegistrationService
 
 import scala.concurrent.Future
@@ -33,28 +36,22 @@ class RegistrationControllerSpec extends SpecBase {
   val mockService: RegistrationService       = mock[RegistrationService]
   val testController: RegistrationController = new RegistrationController(cc, fakeAuthAction, mockService)
 
-  val testApiResponseBody: JsValue = Json.toJson(
-    RegisterIndWithIdAPIResponse(
-      responseCommon = ResponseCommon(status = "200"),
-      responseDetail = ResponseDetail(
-        SAFEID = "Test-SafeId",
-        address = testAddressResponse,
-        individual = IndividualResponse(firstName = "Katie", lastName = "Long", middleName = None)
-      )
-    )
+  val testServiceResponseSuccess = RegisterIndWithIdFrontendResponse(
+    safeId = "test-SafeId",
+    firstName = "Alex",
+    lastName = "Hamilton",
+    middleName = Some("Mathilda"),
+    address = testAddressResponse
   )
 
-  val testRequest: JsValue = Json.toJson(
-    RegisterIndWithIdAPIRequest(
-      requestCommon =
-        RequestCommon(acknowledgementReference = "test-Ref", receiptDate = "test-Date", regime = "test-Regime"),
-      requestDetail = RequestDetailIndividual(
-        requiresNameMatch = true,
-        IDNumber = "test-IDNumber",
-        IDType = "test-IDType",
-        individual = IndividualDetails(dateOfBirth = "test-DOB", firstName = "Professor", lastName = "Rowan"),
-        isAnAgent = false
-      )
+  val testFrontendRequestJson: JsValue = Json.toJson(
+    RegisterIndWithIdFrontendRequest(
+      requiresNameMatch = true,
+      IDNumber = "test-Nino",
+      IDType = "NINO",
+      dateOfBirth = "test-DOB",
+      firstName = "Alex",
+      lastName = "Hamilton"
     )
   )
 
@@ -74,12 +71,29 @@ class RegistrationControllerSpec extends SpecBase {
 
   "RegistrationController" - {
     "registerIndividualWithId" - {
-      "must return response from service" in {
-        when(mockService.registerIndividualWithId(any())).thenReturn(???)
+      "must return success response when the service can retrieve a business partner record" in {
+        when(mockService.registerIndividualWithId(any())(any())).thenReturn(Future(Right(testServiceResponseSuccess)))
 
-        val result = testController.registerIndividualWithId()(fakeRequestWithJsonBody(testRequest))
+        val result = testController.registerIndividualWithId()(fakeRequestWithJsonBody(testFrontendRequestJson))
 
-        result.toString mustBe Future.successful(???).toString
+        status(result) mustBe OK
+        contentAsJson(result) mustBe Json.toJson(testServiceResponseSuccess)
+      }
+      "must return not found response when the service cannot retrieve a business partner record" in {
+        when(mockService.registerIndividualWithId(any())(any())).thenReturn(Future(Left(NotFoundError)))
+
+        val result = testController.registerIndividualWithId()(fakeRequestWithJsonBody(testFrontendRequestJson))
+
+        status(result) mustBe NOT_FOUND
+        contentAsString(result) must include("Could not find or create a business partner record for this user")
+      }
+      "must return internal server error response when the service returns an unexpected error" in {
+        when(mockService.registerIndividualWithId(any())(any())).thenReturn(Future(Left(InternalServerError)))
+
+        val result = testController.registerIndividualWithId()(fakeRequestWithJsonBody(testFrontendRequestJson))
+
+        status(result) mustBe INTERNAL_SERVER_ERROR
+        contentAsString(result) must include("Unexpected error")
       }
       "must return bad request when the request is not valid" in {
         val result = testController.registerIndividualWithId()(fakeRequestWithJsonBody(Json.toJson("invalid timmy")))
