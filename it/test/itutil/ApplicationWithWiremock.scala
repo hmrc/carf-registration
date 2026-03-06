6b2c1072ecd711487229b26b96e5473524984483
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-03-06 14:42:27 +0000

    missing comma fix

--- it/test/itutil/ApplicationWithWiremock.scala
+++ it/test/itutil/ApplicationWithWiremock.scala
@@ -38,7 +38,7 @@ trait ApplicationWithWiremock
       "microservice.services.register-with-id.host"    -> WireMockConstants.stubHost,
       "microservice.services.register-with-id.port"    -> WireMockConstants.stubPort,
       "microservice.services.create-subscription.host" -> WireMockConstants.stubHost,
-      "microservice.services.create-subscription.port" -> WireMockConstants.stubPort
+      "microservice.services.create-subscription.port" -> WireMockConstants.stubPort,
       "microservice.services.auth.host"                -> WireMockConstants.stubHost,
       "microservice.services.auth.port"                -> WireMockConstants.stubPort,
       "microservice.services.register-with-id.host"    -> WireMockConstants.stubHost,
